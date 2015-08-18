6fa18c33874f2929d1bb1f3a6492309f7874ae56
Neel Upadhyaya <neel.upadhyaya@eckoh.com>
2015-08-18 11:24:03 +0100

    Added first version

---
+++ README.md
@@ -0,0 +1,10 @@
+Stomp Tap
+========
+
+Easily print the contents of a stomp queue or topic.
+
+## Usage
+```sh
+npm install
+node index.js stomp://user:pass@host:port/vhost#queue-path
+```
---
+++ index.js
@@ -0,0 +1,31 @@
+require('string.prototype.repeat');
+var prettyjson = require('prettyjson');
+var Stomp = require('stomp-client');
+var Url = require('url');
+
+function main(urn) {
+  var info = Url.parse(urn);
+  var destination = info.hash.substr(1);
+  var vhost = info.path.substr(1);
+  var auth = info.auth.split(/:/);
+  var client = new Stomp(info.hostname, info.port || 61613, auth[0], auth[1], '1.1', vhost);
+
+  client.connect(function(sessionId) {
+    console.log('Connected');
+    client.subscribe(destination, function(body, headers) {
+      var jsBody;
+      var heading = "-".repeat(process.stdout.columns / 2);
+      console.log(heading);
+      console.log(prettyjson.render(headers));
+      console.log(heading);
+      console.log(body);
+      try {
+        jsBody = JSON.parse(body);
+        console.log(prettyjson.render(jsBody));
+      } catch (e) {
+      }
+    });
+  });
+}
+
+main(process.argv[2]);
